73d75e176ad479dab1ee2c8459c8a6fa7722ff9a
Brian Sinclair <brianarn@gmail.com>
2024-09-03 22:04:43 -0600

    Some cleanup, added square and empty

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,29 +1,35 @@
 import "./Widget.scss";
 import BaseCircle from "./BaseCircle";
 import CircleWedge from "./wedges/CircleWedge";
+import SquareWedge from "./wedges/SquareWedge";
+import EmptyWedge from "./wedges/EmptyWedge";
 
 function Widget({ size = 100, viewBoxSize = 100 }) {
     const halfViewSize = viewBoxSize / 2;
     const viewBox = `0 0 ${viewBoxSize} ${viewBoxSize}`;
 
-    const innerScale = 95;
+    const innerScale = 80;
     const innerTranslate = (100 - innerScale) / 2;
 
     const quarters = [
         {
             location: "upperLeft",
+            type: SquareWedge,
             transform: `rotate(-90 ${halfViewSize} ${halfViewSize})`,
         },
         {
             location: "upperRight",
+            type: CircleWedge,
             transform: `translate(100 0)`,
         },
         {
             location: "lowerRight",
+            type: SquareWedge,
             transform: `rotate(90 ${halfViewSize} ${halfViewSize}) translate(100 -100)`,
         },
         {
             location: "lowerLeft",
+            type: EmptyWedge,
             transform: `rotate(180 ${halfViewSize} ${halfViewSize}) translate(0 -100)`,
         },
     ];
@@ -43,8 +49,9 @@ function Widget({ size = 100, viewBoxSize = 100 }) {
                 }) translate(${innerTranslate} ${innerTranslate})`}
             >
                 {quarters.map((quarter) => {
+                    const QuarterType = quarter.type;
                     return (
-                        <CircleWedge
+                        <QuarterType
                             key={quarter.location}
                             size={viewBoxSize}
                             scale={0.5}
